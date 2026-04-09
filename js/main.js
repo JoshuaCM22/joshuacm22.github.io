642d9b35b1b7009ca201417805760038ceed4428
Joshua C. Magoliman <57259934+JoshuaCM22@users.noreply.github.com>
2026-04-09 22:23:47 +0800

    Added faded animation

--- js/main.js
+++ js/main.js
@@ -10,7 +10,73 @@
   });
 
   $(document).ready(function () {
+    var centerRevealSectionSelectors = [
+      "#desktopOldProjects",
+      "#webOldProjects",
+      "#webProjects",
+      "#desktopProjects",
+      "#mobileProjects",
+    ];
+    var isPersonalProjectsPage =
+      $("#desktopOldProjects").length > 0 && $("#mobileProjects").length > 0;
+    var revealCards = [];
+
+    if (isPersonalProjectsPage) {
+      var revealDelayStep = 0.08;
+      revealCards = Array.prototype.slice.call(
+        document.querySelectorAll(".wrapper.style2 .row > [class*='col-']")
+      );
+    } else {
+      revealCards = Array.prototype.slice.call(
+        document.querySelectorAll(
+          centerRevealSectionSelectors
+            .map(function (selector) {
+              return selector + " .row > [class*='col-']";
+            })
+            .join(", ")
+        )
+      );
+    }
+
+    if (revealCards.length > 0) {
+      document.body.classList.add("js-center-reveal");
+
+      revealCards.forEach(function (card, index) {
+        card.classList.remove("wow", "fadeInLeft", "fadeInRight");
+        card.classList.add("reveal-on-center");
+        card.style.transitionDelay =
+          ((index % 3) * 0.08).toFixed(2) + "s";
+      });
+
+      if ("IntersectionObserver" in window) {
+        var observer = new IntersectionObserver(
+          function (entries, currentObserver) {
+            entries.forEach(function (entry) {
+              if (!entry.isIntersecting) {
+                return;
+              }
+
+              entry.target.classList.add("is-revealed");
+              currentObserver.unobserve(entry.target);
+            });
+          },
+          {
+            threshold: 0.15,
+            rootMargin: "-40% 0px -40% 0px",
+          }
+        );
+
+        revealCards.forEach(function (card) {
+          observer.observe(card);
+        });
+      } else {
+        revealCards.forEach(function (card) {
+          card.classList.add("is-revealed");
+        });
+      }
+    }
+
     new WOW().init();
-});
+  });
   
 })(jQuery);
